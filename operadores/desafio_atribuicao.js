34b0e811059410fe4822540ee9852639b78c1a2b
Maya25 <mayaratalyta25@gmail.com>
2023-08-03 23:20:47 -0300

    Apredendo JavaScript

---
+++ operadores/desafio_atribuicao.js
@@ -0,0 +1,9 @@
+let texto = 'Lista de Aprovados\n';
+texto += '==================\n\n';
+texto += '1. Ana Silva\n';
+texto +=' 2. João Pessoa\n' ;
+texto +=' 3. Pedro Albuquerque\n' ;
+texto +=' 4. Guilherme Pereira\n' ;
+
+console.log(texto);
+
